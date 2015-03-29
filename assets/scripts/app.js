31d7662525847c98751a2ad4d2aefd0a5d4ed996
Kyle Bradshaw <kyle@somedirection.com>
2015-03-29 14:26:06 -0400

    more manual way but works

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 angular
     .module('madLibs', [])
     .controller('mainController', ['$scope', function($scope) {
+        //unused
         $scope.words = ['male name', 'Dirty task', 'Obnoxious celebrity', 'Job title', 'Celebrity', 'Huge number', 'Tedious task', 'Useless skill', 'Adjective']
     }]);
