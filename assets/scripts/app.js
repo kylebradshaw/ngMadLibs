ce44a448e6941aaf327614955d46be1960db71ec
Kyle Bradshaw <kyle@somedirection.com>
2015-04-03 11:47:45 -0400

    pronouns added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,4 +4,16 @@ angular
         $scope.placeholders = ['female name', 'job title', 'tedious task', 'dirty task', 
             'celebrity', 'useless skill', 'adjective', 'obnoxious celebrity', 'huge number', 'male name'];
         $scope.words = [];
+
+        $scope.$watch('gender', function(newvalue, oldvalue) {
+            if (newvalue === 'female') {
+                $scope.subjectPronoun = 'she';
+                $scope.objectPronoun = 'her';
+                $scope.words['name'] = 'female name';
+            } else {
+                $scope.subjectPronoun = 'he';
+                $scope.objectPronoun = 'his';
+                $scope.words['name'] = 'male name';
+            }
+        });
     }]);
