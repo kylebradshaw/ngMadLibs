5a4aee0e7391fe089c1735e203dc7ec92a9a722c
Kyle Bradshaw <kyle@somedirection.com>
2015-04-04 10:13:17 -0400

    better validation and UX

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 ![smiley](https://www.dropbox.com/s/5nbvdl5w3b4zkfh/mad-lib-happy-face.png?dl=1)
 
 ![page](https://tf-curricula-prod.s3.amazonaws.com/curricula/TK7MJkkxuVSLEH2iz0I7/ANG-001/v2/assets2/1.2.6_ngMadLibs/ngmadlib-mockup.png)
+
+## Refactor2
+
+*On page load, the user should be presented with a UI like the first sketch above.
+*When they click "Generate My Mad Lib," the app should validate that the user has supplied a value for each of the text inputs. If the user is missing an input, the app should provide feedback about this. (It's up to you to define what that content is and how and where it should appear.)
+*All inputs are required.
+*Validate the huge number field to ensure the user has input a number.
+*If submitted form passes validation, the first view should be hidden, and the user should see the second view, with the word values they plugged in displayed in the appropriate locations.
+*At the bottom of the Mad Lib display interface, there should be a reset button that the user can click to start over. Without refreshing the page, this button should take the user back to the first view, and zero out the word values.
+
+1. roll back to individual inputs
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,29 +1,50 @@
+function unpack(input) {
+    return input.split(/(?=[A-Z])/).join(' ').toLowerCase();
+};
+
 angular
     .module('madLibs', [])
     .controller('mainController', ['$scope', function($scope) {
 
-        $scope.words = {
-            'name': 'female name',
+        var words = {
+            'name': 'name',
             'jobTitle': 'job title',
             'tediousTask': 'tediuous task',
             'dirtyTask': 'dirty task',
             'celebrity': 'celebrity',
             'uselessSkill': 'useless skill',
             'adjective': 'adjective',
-            'obnoxious': 'obnoxious celebrity',
+            'obnoxiousCelebrity': 'obnoxious celebrity',
             'hugeNumber': 'huge number'
         };
 
-        $scope.$watch('gender', function(newvalue, oldvalue) {
-            if (newvalue === 'female') {
-                $scope.subjectPronoun = 'she';
-                $scope.objectPronoun = 'her';
-                $scope.words['name'] = 'female name';
-            } else {
-                $scope.subjectPronoun = 'he';
-                $scope.objectPronoun = 'his';
-                $scope.words['name'] = 'male name';
-            }
-        }, true);
+        // http://goo.gl/fnsO2P
+        var resetWords = JSON.parse(JSON.stringify(words));
 
-    }]);
+        $scope.words = words;
+        $scope.subjectPronoun = 'he/she';
+        $scope.objectPronoun = 'his/her';
+
+        $scope.submit = function(){
+            if($scope.madLibForm.$valid) {
+                $scope.isSubmitted = true;
+            }
+        };
+
+        //better way to do reset?
+        $scope.reset = function() {
+            var object = resetWords;
+            for (var property in object) {
+                if(object.hasOwnProperty(property)){
+                    $scope.words[property] = unpack(object[property]);
+                }
+            }
+            $scope.isSubmitted = !$scope.isSubmitted;
+        }
+
+    }])
+    .filter('unpack', function(){
+        return function(input){
+            return unpack(input);
+        }
+    });
